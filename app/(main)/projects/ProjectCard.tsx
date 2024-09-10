0ad86a7f6026922c8f36fef98bd7e88934a34b4f
Mark Sun <50621638+WeepsDanky@users.noreply.github.com>
2024-09-10 22:32:12 +0800

    Update ProjectCard.tsx

--- app/(main)/projects/ProjectCard.tsx
+++ app/(main)/projects/ProjectCard.tsx
@@ -42,7 +42,7 @@ export function ProjectCard({ project }: { project: Project }) {
     >
       <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
         <Image
-          src={urlForImage(icon)?.size(100, 100).auto('format').url() || '/favicon-32x32.png'}
+          src={icon ? urlForImage(icon)?.size(100, 100).auto('format').url() : '/favicon-32x32.png'}
           alt=""
           width={36}
           height={36}
